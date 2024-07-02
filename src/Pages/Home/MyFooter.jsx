c696263dcff4590fbfa3696f3fec1d5346664f14
Mujahid2000 <your.email@example.com>
2024-07-02 15:41:54 +0600

    add footer code

--- src/Pages/Home/MyFooter.jsx
+++ src/Pages/Home/MyFooter.jsx
@@ -9,7 +9,7 @@ const MyFooter = () => {
     <div className="xl:grid xl:grid-cols-3 xl:gap-8">
       <div className="space-y-8">
         <img className="h-10" src="https://i.ibb.co/pPQVxSw/Screenshot-2024-06-16-214828-removebg-preview.png" alt="Company name"/>
-        <p className="text-sm leading-6 text-white">Highlight benefits of each exercise, both physical and mental</p>
+        <h2 className="text-lg font-semibold leading-6 text-white">Gym Center</h2>
         <div className="flex space-x-6">
         <a href="https://www.facebook.com" className="hover:text-white">
               <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" className="h-6 w-6" />
@@ -24,14 +24,14 @@ const MyFooter = () => {
               <img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="GitHub" className="h-6 w-6" />
             </a>
             <a href="https://www.youtube.com" className="hover:text-white">
-              <img src="https://cdn-icons-png.flaticon.com/512/733/733646.png" alt="YouTube" className="h-6 w-6" />
+              <img src="https://i.ibb.co/JQMHLvP/You-Tube-14.png" alt="YouTube" className="h-6 w-8" />
             </a>
         </div>
       </div>
       <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
         <div className="md:grid md:grid-cols-2 md:gap-8">
           <div>
-            <h3 className="text-sm font-semibold leading-6 text-white">Solutions</h3>
+            <h3 className="text-base font-semibold leading-6 text-white">Solutions</h3>
             <ul role="list" className="mt-6 space-y-4">
               <li>
                 <a  className="text-sm cursor-pointer leading-6 text-white hover:text-white">Marketing</a>
@@ -48,7 +48,7 @@ const MyFooter = () => {
             </ul>
           </div>
           <div className="mt-10 md:mt-0">
-            <h3 className="text-sm font-semibold leading-6 text-white">Support</h3>
+            <h3 className="text-base font-semibold leading-6 text-white">Support</h3>
             <ul role="list" className="mt-6 space-y-4">
               <li>
                 <a  className="text-sm cursor-pointer leading-6 text-white hover:text-white">Pricing</a>
@@ -67,7 +67,7 @@ const MyFooter = () => {
         </div>
         <div className="md:grid md:grid-cols-2 md:gap-8">
           <div>
-            <h3 className="text-sm font-semibold leading-6 text-white">Company</h3>
+            <h3 className="text-base font-semibold leading-6 text-white">Company</h3>
             <ul role="list" className="mt-6 space-y-4">
               <li>
                 <a  className="text-sm cursor-pointer leading-6 text-white hover:text-white">About</a>
@@ -81,13 +81,11 @@ const MyFooter = () => {
               <li>
                 <a  className="text-sm cursor-pointer leading-6 text-white hover:text-white">Press</a>
               </li>
-              <li>
-                <a  className="text-sm cursor-pointer leading-6 text-white hover:text-white">Partners</a>
-              </li>
+              
             </ul>
           </div>
           <div className="mt-10 md:mt-0">
-            <h3 className="text-sm font-semibold leading-6 text-white">Legal</h3>
+            <h3 className="text-base font-semibold leading-6 text-white">Legal</h3>
             <ul role="list" className="mt-6 space-y-4">
               <li>
                 <a  className="text-sm cursor-pointer leading-6 text-white hover:text-white">Claim</a>
@@ -97,6 +95,9 @@ const MyFooter = () => {
               </li>
               <li>
                 <a  className="text-sm cursor-pointer leading-6 text-white hover:text-white">Terms</a>
+              </li>
+              <li>
+                <a  className="text-sm cursor-pointer leading-6 text-white hover:text-white">Partners</a>
               </li>
             </ul>
           </div>
